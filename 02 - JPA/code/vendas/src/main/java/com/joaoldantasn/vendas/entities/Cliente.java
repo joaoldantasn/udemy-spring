fd86be6adf5339fa1269be61552f04215a99589d
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-03-25 21:54:51 -0300

    persistindo com EntityManager

--- 02 - JPA/code/vendas/src/main/java/com/joaoldantasn/vendas/entities/Cliente.java
+++ 02 - JPA/code/vendas/src/main/java/com/joaoldantasn/vendas/entities/Cliente.java
@@ -1,10 +1,30 @@
 package com.joaoldantasn.vendas.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
 public class Cliente {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	private String nome;
 	
+	public Cliente() {
+		
+	}
+
+	public Cliente(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
 	public Integer getId() {
 		return id;
 	}
