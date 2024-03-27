5c888c588e931967f716dfaa8aa41e023b34e405
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-03-27 20:28:03 -0300

    Exception Handlers

---
+++ 03 - CRUD/code/vendas/src/main/java/com/joaoldantasn/vendas/controllers/ApplicationControllerAdvice.java
@@ -0,0 +1,21 @@
+package com.joaoldantasn.vendas.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.joaoldantasn.vendas.exceptions.ApiErrors;
+import com.joaoldantasn.vendas.exceptions.RegraDeNegocioException;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+	
+	@ExceptionHandler(RegraDeNegocioException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ApiErrors handleRegraNegocioException(RegraDeNegocioException ex) {
+		String mensagemErro = ex.getMessage();
+		return new ApiErrors(mensagemErro);
+	}
+	
+}
---
+++ 03 - CRUD/code/vendas/src/main/java/com/joaoldantasn/vendas/exceptions/ApiErrors.java
@@ -0,0 +1,18 @@
+package com.joaoldantasn.vendas.exceptions;
+
+import java.util.Arrays;
+import java.util.List;
+
+import lombok.Getter;
+
+
+public class ApiErrors {
+	
+	@Getter
+	private List<String> erros;
+	
+	public ApiErrors(String mensagemErro) {
+		this.erros = Arrays.asList(mensagemErro);
+	}
+	
+}
