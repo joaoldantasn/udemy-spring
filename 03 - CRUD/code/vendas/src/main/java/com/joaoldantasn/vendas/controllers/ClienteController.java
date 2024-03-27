2a36bc7b99e7c9bf0ea90dde0ade3607fb8c8c26
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-03-27 10:11:39 -0300

    GetMapping

--- 03 - CRUD/code/vendas/src/main/java/com/joaoldantasn/vendas/controllers/ClienteController.java
+++ 03 - CRUD/code/vendas/src/main/java/com/joaoldantasn/vendas/controllers/ClienteController.java
@@ -1,14 +1,25 @@
 package com.joaoldantasn.vendas.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.joaoldantasn.vendas.entities.Cliente;
+import com.joaoldantasn.vendas.repositories.ClienteRepository;
+
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
+	
+	@Autowired
+	private ClienteRepository clientes;
 
 	@RequestMapping(value ="/hello/{nome}", method = RequestMethod.GET)
 	@ResponseBody
@@ -16,4 +27,13 @@ public class ClienteController {
 		return String.format("Hello %s", nomeCliente);
 	}
 	
+	@GetMapping("{id}")
+	@ResponseBody
+	public ResponseEntity<Cliente> getClienteById(@PathVariable("id") Integer id) {
+		Optional<Cliente> cliente =  clientes.findById(id);
+		if(cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
 }
