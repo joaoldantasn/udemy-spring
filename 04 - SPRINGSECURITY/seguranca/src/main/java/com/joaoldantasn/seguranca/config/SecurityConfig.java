2ec1754090bb0dd3738119fe393a6b955ae17141
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-04-04 09:10:50 -0300

    conf spring security ainda não consegue acessar so protegeu erro403)

---
+++ 04 - SPRINGSECURITY/seguranca/src/main/java/com/joaoldantasn/seguranca/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.joaoldantasn.seguranca.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+		return http
+				.authorizeHttpRequests(customizer -> {
+					customizer.requestMatchers("/public").permitAll();
+					
+					//so pode ser chamado por ultimo
+					customizer.anyRequest().authenticated();
+				})
+				.build();
+	}
+	
+}
