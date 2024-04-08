23ebf5f9b66763dba03f7d742e141f286ba859ce
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-04-08 11:09:18 -0300

    criando dominio para controle de usuarios

---
+++ 04 - SPRINGSECURITY/seguranca/src/main/java/com/joaoldantasn/seguranca/entities/UsuarioGrupo.java
@@ -0,0 +1,27 @@
+package com.joaoldantasn.seguranca.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class UsuarioGrupo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private String id;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_usuario")
+	private Usuario usuario;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_grupo")
+	private Grupo grupo;
+	
+}
