ebf12a7f2bbc05b61bd998bf436a257840bb155b
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-04-08 14:35:22 -0300

    Cadastro de grupos

---
+++ 04 - SPRINGSECURITY/seguranca/src/main/java/com/joaoldantasn/seguranca/repositories/GrupoRepository.java
@@ -0,0 +1,9 @@
+package com.joaoldantasn.seguranca.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.joaoldantasn.seguranca.entities.Grupo;
+
+public interface GrupoRepository extends JpaRepository<Grupo, String> {
+
+}
